3d173c0a6b9584fc8400aa4e62b4fcca49548095
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-10-06 23:12:38 -0400

    Successfully grabs Names from the webpage and creates a csv file with those names, and locations to put the rest of their information.
    DOES NOT do any checking to see if the webpage is valid!!!

--- WebScrape.py
+++ WebScrape.py
@@ -1,3 +1,5 @@
+import csv
+
 import requests as req
 from bs4 import BeautifulSoup as BSup
 
@@ -9,9 +11,26 @@ class WebScraper:
         url = "https://www.shepherd.edu/"+department+"/staff"
         self.page = req.get(url)
         self.soup = BSup(self.page.content, "html.parser")
+        self.scrape()
+
 
     def scrape(self):
-        pass
+        content = self.soup.find(class_="content")
+        staffmembers = content.find_all("div", class_="staff-item")
+        csv_data=[]
+        headers = ["Name", "Title", "Email", "Phone", "Bio"]
+        for item in staffmembers:
+            memberinfo = ['None', 'None', 'None', 'None', 'None']
+            lines = item.find_all('tr')
+            name = item.find('h2').text
+            memberinfo[0] = name
+            csv_data.append(memberinfo)
+        with open('FacultyInfo.csv', 'w', newline='') as fi:
+            wr = csv.writer(fi)
+            wr.writerow(headers)
+            wr.writerows(csv_data)
+
+
 
 if __name__ == '__main__':
     ws = WebScraper()
